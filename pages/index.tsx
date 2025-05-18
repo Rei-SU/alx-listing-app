e96e8e7be15b333cbeead6c3c483fc4615fb0d45
Anas Faouzi <anas123faouzi@gmail.com>
2025-05-18 05:58:12 +0100

    wlcom to ALX listing app

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to ALX Listing App 🚀
+      </h1>
+    </main>
+  );
+};
+
+export default Home;
